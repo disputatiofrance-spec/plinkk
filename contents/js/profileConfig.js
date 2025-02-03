119096b0e51d3ecef4317e66cc432a0c46d3f12b
NeptuneOwwO <neptune@studio.klaynight.fr>
2025-02-03 05:12:50 +0100

    discord box icon change

--- contents/js/profileConfig.js
+++ contents/js/profileConfig.js
@@ -1,5 +1,5 @@
-const selectedAnimationIndex = 0; // Change this value to select a different animation
-const selectedAnimationButtonIndex =2;
+const selectedAnimationIndex = 2; // Change this value to select a different animation
+const selectedAnimationButtonIndex = 2;
 // 0 : Fondu
 // 1 : Glisser
 // 2 : Zoomer
@@ -32,16 +32,16 @@ const selectedThemeIndex = 14; // Changez cette valeur pour sélectionner un aut
     const profileData = {
         profileLink: "https://discord.gg/uFZXwWdewc",
         profileImage: "https://www.nautiljon.com/images/perso/00/57/bachira_meguru_21375.webp",
-        profileIcon: "https://cdn.discordapp.com/icons/1197867213110132748/a_833bd9dc0d68996f7f5a3088697a3e25.png?size=256",
+        profileIcon: "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg",
         profileSiteText: "",
         userName: "NeptuneOWO",
         email: "neptune@studio.klaynight.fr",
         links: [
             { icon: "https://tr.rbxcdn.com/30DAY-Avatar-D93F7C7A9A55F82FFEF82E48C6A86E6E-Png/352/352/Avatar/Webp/noFilter",url: "https://www.roblox.com/users/3261357291/profile", text: "roblox" },
             { icon: "https://cliply.co/wp-content/uploads/2021/02/372102690_TIKTOK_LOGO_1080.png",url: "https://www.tiktok.com/@neptuneowo_", text: "tiktok" },
-            { icon: "https://cdn.discordapp.com/attachments/1317863719161561179/1335382549765165106/efjz.jpg?ex=679ff76a&is=679ea5ea&hm=1b01411b619ff15ef36ceed583ef950234def5f2eacd446645eab4b0280ba048&",url: "https://discord.gg/5EFfn3pZSt", text: "SL brm5" }
+            { icon: "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg",url: "https://discord.gg/5EFfn3pZSt", text: "Shadow Legion" }
         ],
-        backgroundImage: "https://static.vecteezy.com/ti/vecteur-libre/p1/12697876-motif-geometriquele-continue-noir-et-blanc-motif-repetitif-monochrome-arriere-plan-abstrait-optique-tridimensionnel-avec-cubes-troues-vectoriel.jpg",
+        backgroundImage: "https://previews.123rf.com/images/noppanun/noppanun1409/noppanun140900089/31830302-seamless-pattern-g%C3%A9om%C3%A9trique-noir-et-blanc-avec-la-ligne-et-l-hexagone.jpg",
         profileHoverColor: "",
         neonColors: ["", "", "", "", ""],
         iconUrl: "https://www.nautiljon.com/images/perso/00/57/bachira_meguru_21375.webp"
