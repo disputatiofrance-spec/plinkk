2dafcd002a30bce90d4f0b0936732be3d90e7270
Klaynight-dev <63601267+Klaynight-dev@users.noreply.github.com>
2025-02-05 15:48:47 +0100

    Change UI exemple

--- contents/js/profileConfig.js
+++ contents/js/profileConfig.js
@@ -22,17 +22,17 @@ const profileData = {
 
     backgroundSize : 50, // En pourcentage
     
-    selectedThemeIndex: 2,                  // Thème sélectionné            (voir ci-dessous)
+    selectedThemeIndex: 5,                   // Thème sélectionné            (voir ci-dessous)
 
     selectedAnimationIndex : 13,             // Animation de l'article       (voir ci-dessous)
     selectedAnimationButtonIndex : 10,       // Animation des boutons        (voir ci-dessous)
-    selectedAnimationBackgroundIndex: 0,    // Animation de l'arrière-plan  (voir ci-dessous)
+    selectedAnimationBackgroundIndex: 0,     // Animation de l'arrière-plan  (voir ci-dessous)
 
     animationDurationBackground: 30,  // Durée de l'animation en secondes
     delayAnimationButton: 0.1,        // Délai de l'animation en secondes
 
     canvaEnable: 1, // 1 : Enable, 0 : Disable
-    selectedCanvasIndex: 5
+    selectedCanvasIndex: 1
 
 };
 //      Thèmes      \\
